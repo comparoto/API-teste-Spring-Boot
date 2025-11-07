955e96f9686b1a18db2aa377d29828ded5dfb5e5
Juliana Comparoto  ᓚ₍⑅^..^₎♡ <jcb5@cesar.school>
2025-11-07 17:57:32 -0300

    Update ProtegeAgroApiApplication.java

--- ProtegeAgroApiApplication.java
+++ ProtegeAgroApiApplication.java
@@ -1,6 +1,5 @@
 package com.protegeagro.protege_agro_api;
 
-import com.protegeagro.protege_agro_api.model.Usuario;
 import com.protegeagro.protege_agro_api.repository.UsuarioRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -19,21 +18,7 @@ public class ProtegeAgroApiApplication {
     CommandLineRunner run(UsuarioRepository usuarioRepository, PasswordEncoder passwordEncoder) {
         return args -> {
 
-            if (usuarioRepository.findByEmail("admin@admin.com").isEmpty()) {
-
-                Usuario admin = new Usuario();
-                admin.setEmail("admin@admin.com");
-                admin.setSenha(passwordEncoder.encode("123456"));
-
-                admin.setTelefone("000000000");
-                admin.setEstado("Pernambuco");
-                admin.setRegiao("Sertão");
-                admin.setCultivo("Milho");
-                usuarioRepository.save(admin);
-                System.out.println("Usuário de teste 'admin@admin.com' criado com sucesso!");
-            } else {
-                System.out.println("Usuário de teste 'admin@admin.com' já existe.");
-            }
         };
     }
 }
+
